d3805b74134d1d9dee081f6c8a53a004f6399745
Muhammad Moaz <m-moaz@hotmail.com>
2017-01-14 22:47:35 +0300

    Initial Commit

---
+++ Mars Rover/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mars Rover
+//
+//  Created by Muhammad Moaz on 1/14/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
