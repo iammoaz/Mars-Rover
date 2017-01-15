fc736e4c00a5147be68e4c33c66d7be90add01ee
Muhammad Moaz <m-moaz@hotmail.com>
2017-01-15 12:22:22 +0300

    Initialized Photo object and Adds global constants file

---
+++ Mars Rover/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Mars Rover
+//
+//  Created by Muhammad Moaz on 1/15/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "qpqC7TejUnC7nTNKBtzcdYFx0g4naMrlOGpapxdM"
+    static let BASE_URL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
+}
---
+++ Mars Rover/Photo.swift
@@ -0,0 +1,17 @@
+//
+//  Photo.swift
+//  Mars Rover
+//
+//  Created by Muhammad Moaz on 1/15/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import Foundation
+
+struct Photo {
+    public let imageURL: String
+    
+    private init(imageURL: String) {
+        self.imageURL = imageURL
+    }
+}
