50f6260f5cd69218b1d55dac5ec0b1b5ffc05404
Muhammad Moaz <m-moaz@hotmail.com>
2017-01-15 13:14:04 +0300

    Implements UITableView Datasource Method

--- Mars Rover/ViewController.swift
+++ Mars Rover/ViewController.swift
@@ -12,14 +12,26 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    fileprivate var photos: [Photo]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
 
