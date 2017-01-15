3f5ba5ba448431253f5e3acea3a90efd888c5901
Muhammad Moaz <m-moaz@hotmail.com>
2017-01-15 11:52:03 +0300

    Adds Initial setup of tableView

--- Mars Rover/ViewController.swift
+++ Mars Rover/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
